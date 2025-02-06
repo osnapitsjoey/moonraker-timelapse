5039ff8a90def93d89d74997929ac1434f87fca4
Albert Smith <osnapitsjoey@gmail.com>
2025-02-06 17:34:13 -0500

    installs to my special klipper

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
-#### Moonraker Timelapse component uninstaller
+#### Moonraker Timelapse component installer
 ####
-#### Copyright (C) 2021 till today Christoph Frei <fryakatkop@gmail.com>
-#### Copyright (C) 2021 till today Stephan Wendel aka KwadFan <me@stephanwe.de>
+#### This version installs only into the custom Klipper instance.
+#### Custom Klipper folder: klipper_custom_ender3v3se
+#### Printer configuration folder: printer_Ender3v3se_data (only this one will be used)
+####
+#### Copyright (C) 2021 till today Christoph Frei
+#### Copyright (C) 2021 till today Stephan Wendel
 ####
 #### This file may be distributed under the terms of the GNU GPLv3 license.
 ####
@@ -17,7 +21,7 @@ set -Ee
 
 ### Check non-root
 if [[ ${UID} = "0" ]]; then
-    printf "\n\tYOU DONT NEED TO RUN INSTALLER AS ROOT!\n"
+    printf "\n\tYOU DON'T NEED TO RUN THE INSTALLER AS ROOT!\n"
     printf "\tYou will be prompted for sudo password if needed!\nExiting...\n"
     exit 1
 fi
@@ -29,14 +33,14 @@ SRC_DIR="$( cd "$(dirname "$(dirname "${BASH_SOURCE[0]}")")"/ && pwd )"
 
 ## Initialize global vars and arrays
 DATA_DIR=()
-DEPENDS_ON=( moonraker klipper )
-MOONRAKER_TARGET_DIR="${HOME}/moonraker/moonraker/components"
+# We only depend on Moonraker and the custom Klipper folder.
+DEPENDS_ON=( moonraker klipper_custom_ender3v3se )
 PKGLIST="wget"
 SERVICES=()
 ### END
 
 ## Helper funcs
-### Ask for proceding install (Step 2)
+### Ask for proceeding install (Step 2)
 function continue_install() {
     local reply
     while true; do
@@ -46,11 +50,11 @@ function continue_install() {
                 break
             ;;
             [Nn]* )
-                abort_msg ### See Error messages
+                abort_msg
                 exit 0
             ;;
             * )
-                printf "\033[31mERROR: Please type Y or N !\033[0m"
+                printf "\033[31mERROR: Please type Y or N!\033[0m"
             ;;
         esac
     done
@@ -63,8 +67,7 @@ function initial_check() {
     for i in "${DEPENDS_ON[@]}"; do
         if [[ -d "${HOME}/${i}" ]]; then
             dep_found_msg "${i}"
-        fi
-        if [[ ! -d "${HOME}/${i}" ]]; then
+        else
             dep_not_found_msg "${i}"
         fi
     done
@@ -78,7 +81,7 @@ function initial_check() {
 function get_service_names() {
     for i in "${DEPENDS_ON[@]}"; do
         sudo systemctl list-units --full --all -t service --no-legend \
-        | sed 's/^  //' | grep "^${i}*" | awk -F" " '{print $1}'
+          | sed 's/^  //' | grep "^${i}*" | awk -F" " '{print $1}'
     done
 }
 
@@ -89,14 +92,11 @@ function set_service_name_array() {
     done < <(get_service_names)
 }
 
-## Stop related service (Step 4)
+## Stop related services (Step 4)
 function stop_services() {
     local service
-    ## Create services array
     set_service_name_array
-    ## Dsiplay header message
     stop_service_header_msg
-    ## Stop services
     for service in "${SERVICES[@]}"; do
         stop_service_msg "${service}"
         if sudo systemctl -q is-active "${service}"; then
@@ -112,9 +112,7 @@ function stop_services() {
 ## Start related services (Step 9)
 function start_services() {
     local service
-    ## Dsiplay header message
     start_service_header_msg
-    ## Stop services
     for service in "${SERVICES[@]}"; do
         start_service_msg "${service}"
         if ! sudo systemctl -q is-active "${service}"; then
@@ -128,46 +126,24 @@ function start_services() {
 }
 ### END
 
-### Determine Data sructure (Step 5)
-## Get Instance names, also used for single instance installs
-function get_instance_names() {
-    local instances path
-    instances="$(find "${HOME}" -maxdepth 1 -type d -name "*_data" -printf "%P\n")"
-    while read -r path ; do
-        DATA_DIR+=("${path}")
-    done <<< "${instances}"
-}
-
+### Determine Data structure (Step 5)
+# We only use the custom printer configuration folder.
 function determine_data_structure() {
-    ## See if there is more then 'klipper_config'
-    get_instance_names
-    if [[ -d "${HOME}/klipper_config" ]] &&
-    [[ "${#DATA_DIR[@]}" -eq 1 ]]; then
-        DATA_DIR+=( klipper_config )
-        printf "Old data structure found '%s'" "${DATA_DIR[0]}"
-        return
-    fi
-    if [[ ! -d "${HOME}/klipper_config" ]] &&
-    [[ "${#DATA_DIR[@]}" -eq 1 ]] &&
-    [[ "${DATA_DIR[0]}" == "printer_data" ]] ; then
-        printf "New data structure found '%s' (single instance)\n" "${DATA_DIR[0]}"
-        return
-    fi
-    if [[ ! -d "${HOME}/klipper_config" ]] &&
-    [[ "${#DATA_DIR[@]}" -gt 1 ]] ; then
-        printf "New data structure found ... (Multi instance)\n"
-        return
+    if [[ -d "${HOME}/printer_Ender3v3se_data" ]]; then
+        DATA_DIR=("printer_Ender3v3se_data")
+        printf "Using custom printer configuration folder: '%s'\n" "${DATA_DIR[0]}"
+    else
+        printf "\n\033[31mERROR:\033[0m Custom printer configuration folder 'printer_Ender3v3se_data' not found.\n"
+        exit 1
     fi
 }
 ### END
-
 
 ### Link component (Step 6)
 function link_component() {
     if [ -d "${MOONRAKER_TARGET_DIR}" ]; then
         printf "Linking extension to moonraker ... "
-        if ln -sf "${SRC_DIR}/component/timelapse.py" \
-        "${MOONRAKER_TARGET_DIR}/timelapse.py" &> /dev/null; then
+        if ln -sf "${SRC_DIR}/component/timelapse.py" "${MOONRAKER_TARGET_DIR}/timelapse.py" &> /dev/null; then
             printf "[\033[32mOK\033[0m]\n"
         else
             printf "[\033[31mFAILED\033[0m]\n"
@@ -177,44 +153,23 @@ function link_component() {
 
 ### Link timelapse.cfg (Step 7)
 function link_macro_file() {
-    local src p
+    local src
     src="${SRC_DIR}/klipper_macro/timelapse.cfg"
-    if [[ "${#DATA_DIR[@]}" -eq 1 ]] && [[ "${DATA_DIR[0]}" == "klipper_config" ]]; then
-        link_to_msg "${DATA_DIR[0]}"
-        if ln -sf "${src}" "${HOME}/${DATA_DIR[0]}/timelapse.cfg"; then
-            link_to_ok_msg
-            return
-        else
-            link_to_failed_msg
-            return
-        fi
-    fi
-    if [[ "${#DATA_DIR[@]}" -eq 1 ]] && [[ "${DATA_DIR[0]}" == "printer_data" ]]; then
+    # We only link into the custom printer configuration folder.
+    if [[ "${#DATA_DIR[@]}" -eq 1 ]] && [[ "${DATA_DIR[0]}" == "printer_Ender3v3se_data" ]]; then
         link_to_msg "${DATA_DIR[0]}"
         if ln -sf "${src}" "${HOME}/${DATA_DIR[0]}/config/timelapse.cfg"; then
             link_to_ok_msg
-            return
         else
             link_to_failed_msg
-            return
         fi
-    fi
-    if [[ "${#DATA_DIR[@]}" -gt 1 ]]; then
-        for p in "${DATA_DIR[@]}"; do
-            if [[ -d "${HOME}/${p}/config" ]]; then
-                link_to_msg "${p}"
-                if ln -sf "${src}" "${HOME}/${p}/config/timelapse.cfg"; then
-                    link_to_ok_msg
-                else
-                    link_to_failed_msg
-                fi
-            fi
-        done
+    else
+        printf "\n\033[31mERROR:\033[0m Custom printer configuration folder not set correctly.\n"
+        exit 1
     fi
 }
 
 ### Check for ffmpeg (Step 8)
-## Check if ffmpeg is installed, returns path if installed
 function ffmpeg_installed() {
     local path
     path="$(command -v ffmpeg)"
@@ -244,7 +199,7 @@ function check_ffmpeg() {
                     break
                 ;;
                 * )
-                    printf "\033[31mERROR: Please type Y or N !\033[0m\n"
+                    printf "\033[31mERROR: Please type Y or N!\033[0m\n"
                 ;;
             esac
         done
@@ -257,7 +212,7 @@ function check_ffmpeg() {
 ### Welcome message (Step 1)
 function welcome_msg() {
     printf "\n\033[31mAhoi!\033[0m\n"
-    printf "moonraker-timelapse install routine\n"
+    printf "moonraker-timelapse install routine (Custom Klipper instance only)\n"
     printf "\n\tThis will take some time ...\n\tYou'll be prompted for sudo password if needed!\n"
     printf "\n\033[31m#################### WARNING #####################\033[0m\n"
     printf "Make sure you are \033[31mnot\033[0m printing during install!\n"
@@ -265,9 +220,9 @@ function welcome_msg() {
     printf "\033[31m##################################################\033[0m\n\n"
 }
 
-### Dependencie messages (Step 3)
+### Dependency messages (Step 3)
 function dep_check_msg() {
-    printf "Check for dependencies to use moonraker-timelapse ...\n"
+    printf "Checking dependencies for moonraker-timelapse ...\n"
 }
 
 function dep_found_msg() {
@@ -280,7 +235,7 @@ function dep_not_found_msg() {
 }
 ### END
 
-### Service related msg (Step 4, Step 9)
+### Service-related messages (Steps 4 & 9)
 function stop_service_header_msg() {
     printf "Stopping related service(s) ... \n"
 }
@@ -314,7 +269,7 @@ function service_start_failed_msg() {
 }
 ### END
 
-### Link timelapse.cfg related messages (Step 7)
+### Link timelapse.cfg messages (Step 7)
 function link_to_msg() {
     printf "Linking timelapse.cfg to '%s' ... " "${1}"
 }
@@ -328,9 +283,9 @@ function link_to_failed_msg() {
 }
 ### END
 
-### Install finished message(s) (Step 10)
+### Install finished messages (Step 10)
 function finished_install_msg() {
-    printf "\nmoonraker-timelapse \033[32msuccessful\033[0m installed ...\n"
+    printf "\nmoonraker-timelapse \033[32msuccessfully\033[0m installed for the custom Klipper instance!\n"
     config_hint_header
     config_hint_block
     config_hint_footer
@@ -347,22 +302,10 @@ function config_hint_footer() {
 }
 
 function config_hint_block(){
-    if [[ "${DATA_DIR[0]}" == "klipper_config" ]]; then
-        printf "\n\t- for Printer %s:\n" "${DATA_DIR[0]/_config/}"
-        printf "\t[timelapse]\n\toutput_path: ~/%s/timelapse/\n" "${DATA_DIR[0]}"
-        printf "\tframe_path: /tmp/timelapse\n"
-        return
-    fi
-    for i in "${DATA_DIR[@]}"; do
-        printf "\n\t- for Printer %s:\n" "${i/_data/}"
-        printf "\t[timelapse]\n\toutput_path: ~/%s/timelapse/\n" "${i}"
-        printf "\tframe_path: /tmp/timelapse/%s\n" "${i/_data/}"
-    done
-        if [[ "${#DATA_DIR[@]}" -ne "${#SERVICES[@]}" ]]; then
-            printf "\nFolder structure and service count does not match!\n"
-            printf "Please update only moonraker.conf of the installed printer\n\n"
-        fi
-    return
+    # Hint tailored for the custom printer configuration folder.
+    printf "\n\t- for Printer Ender3v3se (custom instance):\n"
+    printf "\t[timelapse]\n\toutput_path: ~/%s/timelapse/\n" "printer_Ender3v3se_data"
+    printf "\tframe_path: /tmp/timelapse/Ender3v3se\n"
 }
 ### END
 
@@ -377,43 +320,43 @@ function abort_msg() {
 }
 ### END
 
-### MAIN
+## MAIN
+# Moonraker target directory remains unchanged.
+MOONRAKER_TARGET_DIR="${HOME}/moonraker/moonraker/components"
+
 function main() {
+    # Step 1: Print welcome message
+    welcome_msg
 
-# Step 1: Print welcome message
-welcome_msg
+    # Step 2: Ask to proceed
+    continue_install
 
-# Step 2: Ask to proceed
-continue_install
+    # Step 3: Initial dependency checks (moonraker & klipper_custom_ender3v3se)
+    initial_check
 
-# Step 3: Initial checks for dependencies (klipper/moonraker)
-initial_check
+    # Step 4: Stop related services
+    stop_services
 
-# Step 4: Stop related services
-stop_services
+    # Step 5: Set the printer configuration folder (only the custom one)
+    determine_data_structure
 
-# Step 5: Determine data structure
-determine_data_structure
+    # Step 6: Link timelapse component into Moonraker
+    link_component
 
-# Step 6: Link component to $MOONRAKER_TARGET_DIR
-link_component
+    # Step 7: Create symlink for timelapse.cfg into printer_Ender3v3se_data/config
+    link_macro_file
 
-# Step 7: Link timelapse.cfg to $DATA_DIR
-link_macro_file
+    # Step 8: Check for ffmpeg
+    check_ffmpeg
 
-# Step 8: Check for ffmpeg
-check_ffmpeg
+    # Step 9: Restart services
+    start_services
 
-# Step 9: Restart services
-start_services
-
-# Step 10: Print finish message
-finished_install_msg
-
+    # Step 10: Final message
+    finished_install_msg
 }
 
 ## MAIN
-
 main
 exit 0
 
